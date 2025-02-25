7306b3c1cb15cf7ad17fdd09a4b189e378c973b4
zz <frostbolt817@gmail.com>
2025-02-25 15:36:44 +0800

    feat: 添加shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
 import Greet from './greet'
 
 export default function Home() {
@@ -6,6 +8,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Greet />
+        <Button>按钮Caption</Button>
       </main>
     </div>
   );
